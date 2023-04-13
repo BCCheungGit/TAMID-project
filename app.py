f4a89b9bcfbc34a6f5fd7ce50799a737c30b6381
BCCheungGit <87406737+BCCheungGit@users.noreply.github.com>
2023-04-13 21:05:35 +0800

    added flask to app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 
 #dunder main
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+    app1 = create_app(app)
+    app1.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -7,8 +7,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-def create_app():
-    app = Flask(__name__)
+def create_app(app):
     app.config['SECRET_KEY'] = "secretkey"  
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
