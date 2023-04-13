65bbb34c238b04bce75db188412bab169791cf1f
BCCheungGit <87406737+BCCheungGit@users.noreply.github.com>
2023-04-13 21:09:33 +0800

    removed flask from app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+
 # the file that we run to start the website
 from website import create_app
 
-app = Flask(__name__)
 
 #dunder main
 if __name__ == '__main__':
-    app1 = create_app(app)
-    app1.run(debug=True)
+    app = create_app(app)
+    app.run(debug=True)
